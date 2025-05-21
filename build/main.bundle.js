c15c5f6d1a88b85ed618ed2e74c4d52cd72fe7f8
nahye <knh8783@gmail.com>
2025-05-21 11:16:43 +0900

    add mortgageClass

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,64 +1,13 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./js/mortgage.js":
+/***/ "./css/styles.css":
 /*!************************!*\
-  !*** ./js/mortgage.js ***!
+  !*** ./css/styles.css ***!
   \************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   calculateAmortization: () => (/* binding */ calculateAmortization),
-/* harmony export */   calculateMonthlyPayment: () => (/* binding */ calculateMonthlyPayment)
-/* harmony export */ });
-// named export
-
-var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
-  var monthlyRate = 0;
-  if (rate) {
-    monthlyRate = rate / 100 / 12;
-  }
-  var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
-  return {
-    principal: principal,
-    years: years,
-    rate: rate,
-    monthlyPayment: monthlyPayment,
-    monthlyRate: monthlyRate
-  };
-};
-
-// named export
-var calculateAmortization = function calculateAmortization(principal, years, rate) {
-  var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate),
-    monthlyRate = _calculateMonthlyPaym.monthlyRate,
-    monthlyPayment = _calculateMonthlyPaym.monthlyPayment;
-  var balance = principal;
-  var amortization = [];
-  for (var y = 0; y < years; y++) {
-    var interestY = 0; //Interest payment for year y
-    var principalY = 0; //Principal payment for year y
-    for (var m = 0; m < 12; m++) {
-      var interestM = balance * monthlyRate; //Interest payment for month m
-      var principalM = monthlyPayment - interestM; //Principal payment for month m
-      interestY = interestY + interestM;
-      principalY = principalY + principalM;
-      balance = balance - principalM;
-    }
-    amortization.push({
-      principalY: principalY,
-      interestY: interestY,
-      balance: balance
-    });
-  }
-  return {
-    monthlyPayment: monthlyPayment,
-    monthlyRate: monthlyRate,
-    amortization: amortization
-  };
-};
+throw new Error("Module parse failed: Unexpected token (1:5)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n> body {\n|     font-family: 'Roboto', 'Sans Serif';\n|     font-size: 16px;");
 
 /***/ })
 
@@ -89,6 +38,18 @@ var calculateAmortization = function calculateAmortization(principal, years, rat
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
@@ -119,29 +80,61 @@ var calculateAmortization = function calculateAmortization(principal, years, rat
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
+// This entry needs to be wrapped in an IIFE because it needs to be in strict mode.
 (() => {
+"use strict";
 /*!********************!*\
   !*** ./js/main.js ***!
   \********************/
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _mortgage__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./mortgage */ "./js/mortgage.js");
+Object(function webpackMissingModule() { var e = new Error("Cannot find module './mortgageClass'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+/* harmony import */ var _css_styles_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../css/styles.css */ "./css/styles.css");
+/* harmony import */ var _css_styles_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_css_styles_css__WEBPACK_IMPORTED_MODULE_1__);
+//1) * 를 사용하여 모든 함수를 import
+//import * as mortgage from './mortgage'; 
+
+//2) calculateAmortization 함수만 import
+//import { calculateAmortization } from "./mortgage";
+
+//3) calculateAmortization 함수를 as 키워드로 변경하여 import
+//import { calculateAmortization as calcAmortization } from "./mortgage";
+
+//Default export 일 경우 as 키워드 없이 함수명을 변경하여 import 가능함
+//import calcAmortization from "./mortgage";
+
+//Mortgage 클래스 import 하기
+
+
+//css file을 import 하기
 
 document.getElementById('calcBtn').addEventListener('click', function () {
   var principal = document.getElementById("principal").value;
   var years = document.getElementById("years").value;
   var rate = document.getElementById("rate").value;
 
-  // var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
-  var _mortgage$calculateAm = _mortgage__WEBPACK_IMPORTED_MODULE_0__.calculateAmortization(principal, years, rate),
-    monthlyPayment = _mortgage$calculateAm.monthlyPayment,
-    monthlyRate = _mortgage$calculateAm.monthlyRate,
-    amortization = _mortgage$calculateAm.amortization;
+  // let monthlyPayment = calculateMonthlyPayment(principal, years, rate);
+  //let { monthlyPayment, monthlyRate } = calculateMonthlyPayment(principal, years, rate);
+  // 1)
+  //let { monthlyPayment, monthlyRate, amortization } = mortgage.calculateAmortization(principal, years, rate);
+
+  // 2)
+  //let { monthlyPayment, monthlyRate, amortization } = calculateAmortization(principal, years, rate);
+
+  // 3)
+  //let { monthlyPayment, monthlyRate, amortization } = calcAmortization(principal, years, rate);
+
+  //객체생성성
+  var mortgage = new Object(function webpackMissingModule() { var e = new Error("Cannot find module './mortgageClass'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(principal, years, rate);
+  //클래스로 부터 Destructuring Assignment 하기
+  var monthlyPayment = mortgage.monthlyPayment,
+    amortization = mortgage.amortization;
   document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
   document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
-  amortization.forEach(function (month) {
-    return console.log(month);
+  var html = "";
+  mortgage.amortization.forEach(function (year, index) {
+    return html += "\n       <tr>\n           <td>".concat(index + 1, "</td>\n           <td class=\"currency\">").concat(Math.round(year.principalY), "</td>\n           <td class=\"stretch\">\n               <div class=\"flex\">\n                   <div class=\"bar principal\"\n                        style=\"flex:").concat(year.principalY, ";-webkit-flex:").concat(year.principalY, "\">\n                   </div>\n                   <div class=\"bar interest\"\n                        style=\"flex:").concat(year.interestY, ";-webkit-flex:").concat(year.interestY, "\">\n                   </div>\n               </div>\n           </td>\n           <td class=\"currency left\">").concat(Math.round(year.interestY), "</td>\n           <td class=\"currency\">").concat(Math.round(year.balance), "</td>\n       </tr>\n   ");
   });
+  document.getElementById("amortization").innerHTML = html;
 });
 })();
 
